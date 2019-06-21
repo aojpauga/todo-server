3e309e1ab1c4cb3c363c851a13ae0a301a091548
aojpauga <apauga@MSI.localdomain>
2019-06-21 09:41:19 -0600

    Server updated

--- server.js
+++ server.js
@@ -10,7 +10,20 @@ server.use(express.urlencoded({
 }));
 server.use(express.json());
 server.use(cors());
+server.use(function(req, res, next) {
+    console.log(`New request: ${req.method} ${req.path} on ${new Date()}`);
+    next();
+})
 var data = require("./data.js");
+
+//Endpoints
+server.get("/todo", function(req, res) {
+    var response = {
+        msg: "I am here"
+    }
+    res.json(response);
+});
+
 
 server.listen(port, function() {
     console.log(`Listening on port ${port}`);
